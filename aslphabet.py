d6534a32787023275f7546fad45f9c4738a7b6b2
Cookieee20 <142922669+Cookieee20@users.noreply.github.com>
2023-10-10 13:31:42 -0400

    Add files via upload

--- aslphabet.py
+++ aslphabet.py
@@ -1,15 +1,10 @@
-import PIL.Image
 import cv2
-import numpy as np
 import os
-from future.moves import tkinter
-from matplotlib import pyplot as plt
 import mediapipe as mp
-from PIL import Image
 from pytesseract import pytesseract
 import platform
 from tkinter import Tk, Label, Button, Canvas, PhotoImage
-from PIL import Image,ImageTk
+from PIL import Image
 
 # Identify platform and assign to variable
 platform_id = str(platform.system())
@@ -25,16 +20,29 @@ holistic = mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confi
 # Tkinter setup - starter screen
 root = Tk()
 root.title("ASLphabet")
+width =1017
+height=616
+screenwidth = root.winfo_screenwidth()
+screenheight = root.winfo_screenheight()
+align = '%dx%d+%d+%d' % (width, height, (screenheight - width)/2, (screenheight - height)/2)
+root.geometry(align)
 root.resizable(width=False, height=False)
+root.iconbitmap('Logo.ico')
 
 def menu():
-    canvas = Canvas(root, bg="#82A8C9", width=400, height=450)
+    canvas = Canvas(root, bg="#e0f8f8", width=width, height=height)
     canvas.pack()
-    myLabel = Label(canvas, text="   Welcome User!   \n   ------------   ").place(relx = 0.5,  anchor = tkinter.N)
-    myImage = PhotoImage(file="LogoNoBackground.png").subsample(2)
-    canvas.create_image(210, 150, anchor="center" ,image=myImage)
-    myLabel1 = Label(canvas, text="Press Q to quit out of the camera.").place(relx = 0.5, rely = 0.6, anchor = tkinter.CENTER)
-    myButton = Button(canvas, text="Launch Camera", command=camera).place(relx = 0.5, rely = 0.8, anchor = tkinter.CENTER)
+    myLabel = Label(canvas, bg="#e0f8f8", justify="center", font=('Modern', 40, "bold"), text=("Welcome ")+os.getlogin()+"!").place(x=40, y=40, width = 330, height = 68)
+    myImage = PhotoImage(file="LogoNoBackground.png").subsample(1)
+    canvas2 = Canvas(canvas, bg="#e0f8f8", width=508, height=616)
+    canvas2.place(x=507, y=0)
+    canvas2.create_image(250, 308, anchor="center" ,image=myImage)
+    myLabel1 = Label(canvas, bg="#e0f8f8", justify="center", font=('Modern', 15), text="Press Q to quit out of the camera.").place(x=40, y=100, width = 330, height = 68)
+    myLabel1 = Label(canvas, bg="#e0f8f8", justify="center", font=('Modern', 15), text="Brought to you by: ").place(x=40, y=500, width=330, height=68)
+    myButton1 = Button(canvas, bg="#80CC23", font=('Modern', 18, "bold"), justify="center", text="Launch Camera", command=camera).place(x=20, y=190, width = 378, height = 68)
+    myButton2 = Button(canvas, bg="#80CC23", font=('Modern', 18, "bold"), justify="center", text="Instruction", command="").place(x=20, y=270, width=378, height=68)
+    myButton3 = Button(canvas, bg="#80CC23", font=('Modern', 18, "bold"), justify="center", text="ASL Dictionary", command="").place(x=20, y=350, width=378, height=68)
+    myButton4 = Button(canvas, bg="#80CC23", font=('Modern', 18, "bold"), justify="center", text="Settings", command="").place(x=20, y=430, width=378, height=68)
     root.mainloop()
 
 def mediapipe_detection(image):
@@ -57,10 +65,6 @@ def tesseract(imagepath, platform_id):
         path_to_tesseract = r"/bin/tesseract"
     elif platform_id == 'Windows':
         path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-        
-    
-    
-    #imagepath = 'test1.jpg'
 
     # Set the Tesseract executable path
     pytesseract.tesseract_cmd = path_to_tesseract
@@ -96,4 +100,3 @@ def camera():
     menu()
 
 menu()
-#print(results.face_landmarks)
